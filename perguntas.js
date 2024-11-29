98b4c0e3fed5a560ae3263ecd0de6cb7983027dc
Kawan.S <141418453+KawanSouzaofi@users.noreply.github.com>
2024-11-29 20:11:39 -0300

    no paste

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ensino Médio',
+    'Quem é nosso professor de programação?',
+    'É o MIDERSON'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná?',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve o style.css?',
+    'O CSS separa o conteúdo da representação visual do site.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz sucesso em ingles?',
+    'sucesso em ingles é success (suhkses).'
+)
